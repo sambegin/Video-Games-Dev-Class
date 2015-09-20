9e61a54e6f9bc3cde2a50a056c7391bab53fd79f
David <david.arel@outlook.com>
2015-09-19 22:27:22 -0400

    Load map at start of the game.

---
+++ IFT-2103/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject mapPrefab;
+
+	// Use this for initialization
+	void Start () {
+        loadMap();
+	}
+
+    private void loadMap()
+    {
+        //TODO Could use a service locator to register the map eventually
+        Instantiate(mapPrefab, new Vector3(), new Quaternion());
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
