4cd744c0e399beaa11bac9beaf9a85860c936ae8
PhilKelly <phil.kelly.52@gmail.com>
2015-10-04 21:52:18 -0400

    Adding movement WASD and jump(In air controled)... also there is light at the moment but will be changed soon!

---
+++ IFT-2103/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float speed = 20.0f;
+    private float jumpHeight = 15.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private float gravity = 50.0f;
+    void Start()
+    {
+       
+    }
+
+    void FixedUpdate()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+
+        if (controller.isGrounded) {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump")) {
+                moveDirection.y = jumpHeight;
+            } 
+
+
+       }
+        else
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), moveDirection.y, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection.x *= speed;
+            moveDirection.z *= speed;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+       controller.Move(moveDirection * Time.deltaTime);
+
+    }
+
+}
