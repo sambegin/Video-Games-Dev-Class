1d5c28cb1803aeb908382d25bfcb722c6878f35a
David <david.arel@outlook.com>
2015-10-10 22:09:56 -0400

    Weapon is shooting some kind of laser.

---
+++ IFT-2103/Assets/Weapon.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Weapon : MonoBehaviour
+{
+
+    LineRenderer lineRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.lineRenderer = GetComponent<LineRenderer>();
+        this.lineRenderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            shoot();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            stopShooting();
+        }
+    }
+
+    private void shoot()
+    {
+        RaycastHit hitSpot;
+        Vector3 forwardDirection = transform.up;
+        bool asHitOnSomething = Physics.Raycast(transform.position, forwardDirection, out hitSpot);
+        if (asHitOnSomething)
+        {
+            lineRenderer.SetPosition(1, hitSpot.point);
+        }
+        else
+        {
+            lineRenderer.SetPosition(1, forwardDirection*10);
+        }
+        lineRenderer.SetPosition(0, transform.position);
+
+
+        lineRenderer.enabled = true;
+    }
+
+    private void stopShooting()
+    {
+        lineRenderer.enabled = false;
+    }
+
+
+}
