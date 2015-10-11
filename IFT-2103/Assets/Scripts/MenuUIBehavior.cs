cad6553503b9ab5e6c7828260b8042c894e57033
sam_begin <beginsam2@gmail.com>
2015-10-11 14:54:17 -0400

    Basic Main menu

---
+++ IFT-2103/Assets/Scripts/MenuUIBehavior.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuUIBehavior : MonoBehaviour {
+
+
+    void OnMouseEnter()
+    {
+        GetComponent<TextMesh>().color = Color.black;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<TextMesh>().color = Color.white;
+    }
+
+    void OnMouseUp ()
+    {
+       var textComponent = GetComponent<TextMesh>();
+       if (textComponent.tag == "QuitGame")
+        {
+            Application.Quit();
+        }
+        else
+        {
+            Application.LoadLevel(1);
+        }
+       
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
