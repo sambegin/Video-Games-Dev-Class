92ceda4588504dc4f7fdb80bea7790ed314f0fd4
sam_begin <beginsam2@gmail.com>
2015-10-20 13:06:58 -0400

    refactor

---
+++ IFT-2103/Assets/Scripts/MainMenuBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuBehavior : MonoBehaviour {
+
+    void OnMouseEnter()
+    {
+        GetComponent<TextMesh>().color = Color.black;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<TextMesh>().color = Color.white;
+    }
+
+    void OnMouseUp()
+    {
+        var textComponent = GetComponent<TextMesh>();
+        if (textComponent.tag == "QuitGame")
+        {
+            Application.Quit();
+        }
+        else
+        {
+            Application.LoadLevel(1);
+        }
+
+    }
+}
--- IFT-2103/Assets/Scripts/MenuUIBehavior.cs
+++ IFT-2103/Assets/Scripts/MenuUIBehavior.cs
@@ -15,37 +15,5 @@ public class MenuUIBehavior : MonoBehaviour {
     {
         text.color = Color.white;
     }
-
-    void OnMouseEnter()
-    {
-        GetComponent<TextMesh>().color = Color.black;
-    }
-
-    void OnMouseExit()
-    {
-        GetComponent<TextMesh>().color = Color.white;
-    }
-
-    void OnMouseUp ()
-    {
-       var textComponent = GetComponent<TextMesh>();
-       if (textComponent.tag == "QuitGame")
-        {
-            Application.Quit();
-        }
-        else
-        {
-            Application.LoadLevel(1);
-        }
-       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
-    }
+  
 }
