1c362bc2ea764634548129f48f97b8ffbff0cdd5
sam_begin <beginsam2@gmail.com>
2015-10-21 20:19:17 -0400

    esti

--- IFT-2103/Assets/Scripts/MainMenuBehavior.cs
+++ IFT-2103/Assets/Scripts/MainMenuBehavior.cs
@@ -5,56 +5,43 @@ public class MainMenuBehavior : MonoBehaviour {
 
 
     private Vector3 startGameTextHidePosition = new Vector3(-150, 10, -3);
-    private Vector3 selectLevelTextHidePosition = new Vector3(-150, -5, -3);
+    private Vector3 selectStageTextHidePosition = new Vector3(-150, -5, -3);
     private Vector3 quitGameTextHidePosition = new Vector3(150, -20, -3);
 
     private Vector3 startGameTextVisiblePosition = new Vector3(0, 10, -3);
-    private Vector3 selectLevelTextVisiblePosition = new Vector3(0, -5, -3);
+    private Vector3 selectStageTextVisiblePosition = new Vector3(0, -5, -3);
     private Vector3 quitGameTextVisiblePosition = new Vector3(0, -20, -3);
 
-    private bool inSelectLevel;
+    private bool inselectStage;
 
     public float SmoothFactor = 2;
     public Transform startGameTransform;
-    public Transform selectLevelTransform;
+    public Transform selectStageTransform;
     public Transform quitGameTransform;
 
     public void Update ()
     {
-        if (inSelectLevel)
+        if (inselectStage)
         {
             startGameTransform.position = Vector3.Lerp(startGameTransform.position, startGameTextHidePosition, Time.deltaTime * SmoothFactor);
-            selectLevelTransform.position = Vector3.Lerp(selectLevelTransform.position, selectLevelTextHidePosition, Time.deltaTime * SmoothFactor);
+            selectStageTransform.position = Vector3.Lerp(selectStageTransform.position, selectStageTextHidePosition, Time.deltaTime * SmoothFactor);
             quitGameTransform.position = Vector3.Lerp(quitGameTransform.position, quitGameTextHidePosition, Time.deltaTime * SmoothFactor);
         }
     }
 
-
-    void OnMouseEnter()
+    public void QuitGame()
     {
-        GetComponent<TextMesh>().color = Color.black;
+        Application.Quit();
     }
 
-    void OnMouseExit()
+    public void StartNewGame()
     {
-        GetComponent<TextMesh>().color = Color.white;
+        Application.LoadLevel(1);
     }
 
-    void OnMouseUp()
+    public void SelectStage()
     {
-        var textComponent = GetComponent<TextMesh>();
-        if (textComponent.tag == "QuitGame")
-        {
-            Application.Quit();
-        }
-        else if (textComponent.tag == "SelectLevel")
-        {
-            inSelectLevel = true;
-        }
-        else
-        {
-            Application.LoadLevel(1);
-        }
-
+        inselectStage = true;
     }
+
 }
---
+++ IFT-2103/Assets/Scripts/MainMenuUIBehavior.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuUIBehavior : MonoBehaviour {
+
+    public MainMenuBehavior mainMenuBehavior;
+
+    void OnMouseEnter()
+    {
+        GetComponent<TextMesh>().color = Color.black;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<TextMesh>().color = Color.white;
+    }
+
+    void OnMouseUp()
+    {
+        var textComponent = GetComponent<TextMesh>();
+        if (textComponent.tag == "QuitGame")
+        {
+            mainMenuBehavior.QuitGame();
+        }
+        else if (textComponent.tag == "SelectLevel")
+        {
+            mainMenuBehavior.SelectStage();
+        }
+        else
+        {
+            mainMenuBehavior.StartNewGame();
+        }
+    }
+
+}
