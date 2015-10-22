79240e690b22a7b82bb999a75cc302b5e24877be
sam_begin <beginsam2@gmail.com>
2015-10-22 19:56:26 -0400

    m,lknlnjnkjnkjnjknhkjhjh

--- IFT-2103/Assets/Scripts/MainMenuBehavior.cs
+++ IFT-2103/Assets/Scripts/MainMenuBehavior.cs
@@ -5,40 +5,46 @@ public class MainMenuBehavior : MonoBehaviour {
 
 
     private Vector3 startGameTextHidePosition = new Vector3(-150, 10, -3);
-    private Vector3 selectLevelTextHidePosition = new Vector3(-150, -5, -3);
+    private Vector3 selectStageTextHidePosition = new Vector3(-150, -5, -3);
     private Vector3 quitGameTextHidePosition = new Vector3(-150, -20, -3);
     private Vector3 backTextHidePosition = new Vector3(150, -20, -3);
 
     private Vector3 startGameTextVisiblePosition = new Vector3(0, 10, -3);
-    private Vector3 selectLevelTextVisiblePosition = new Vector3(0, -5, -3);
+    private Vector3 selectStageTextVisiblePosition = new Vector3(0, -5, -3);
     private Vector3 quitGameTextVisiblePosition = new Vector3(0, -20, -3);
     private Vector3 backTextVisiblePosition = new Vector3(0, -20, -3);
 
 
-    private bool inSelectLevel;
+    private string statusMenu = "normal";
 
-    public float SmoothFactor = 2;
-    public Transform startGameTransform;
-    public Transform selectLevelTransform;
-    public Transform quitGameTransform;
-    public Transform backTransform;
+    private float Speed = 100;
+    private Transform startGameTransform;
+    private Transform selectStageTransform;
+    private Transform quitGameTransform;
+    private Transform backTransform;
 
     public void Update ()
     {
-        if (inSelectLevel)
+        startGameTransform = GameObject.FindGameObjectWithTag("StartGame").transform;
+        selectStageTransform = GameObject.FindGameObjectWithTag("SelectStage").transform;
+        quitGameTransform = GameObject.FindGameObjectWithTag("QuitGame").transform;
+        backTransform = GameObject.FindGameObjectWithTag("Back").transform;
+        statusMenu = GameObject.FindGameObjectWithTag("MenuStatus").GetComponent<TextMesh>().text;
+
+        if (statusMenu == "selectStage" && startGameTransform.position != startGameTextHidePosition)
         {
-            startGameTransform.position = Vector3.Lerp(startGameTransform.position, startGameTextHidePosition, Time.deltaTime * SmoothFactor);
-            selectLevelTransform.position = Vector3.Lerp(selectLevelTransform.position, selectLevelTextHidePosition, Time.deltaTime * SmoothFactor);
-            quitGameTransform.position = Vector3.Lerp(quitGameTransform.position, quitGameTextHidePosition, Time.deltaTime * SmoothFactor);
-            backTransform.position = Vector3.Lerp(backTransform.position, backTextVisiblePosition, Time.deltaTime * SmoothFactor);
+            startGameTransform.position = Vector3.MoveTowards(startGameTransform.position, startGameTextHidePosition, Time.deltaTime * Speed);
+            selectStageTransform.position = Vector3.MoveTowards(selectStageTransform.position, selectStageTextHidePosition, Time.deltaTime * Speed);
+            quitGameTransform.position = Vector3.MoveTowards(quitGameTransform.position, quitGameTextHidePosition, Time.deltaTime * Speed);
+            backTransform.position = Vector3.MoveTowards(backTransform.position, backTextVisiblePosition, Time.deltaTime * Speed);
         }
-        //else
-        //{
-        //    startGameTransform.position = Vector3.Lerp(startGameTransform.position, startGameTextVisiblePosition, Time.deltaTime * SmoothFactor);
-        //    selectLevelTransform.position = Vector3.Lerp(selectLevelTransform.position, selectLevelTextVisiblePosition, Time.deltaTime * SmoothFactor);
-        //    quitGameTransform.position = Vector3.Lerp(quitGameTransform.position, quitGameTextVisiblePosition, Time.deltaTime * SmoothFactor);
-        //    backTransform.position = Vector3.Lerp(backTransform.position, backTextHidePosition, Time.deltaTime * SmoothFactor);
-        //}
+        else if(statusMenu == "normal" && startGameTransform.position != startGameTextVisiblePosition)
+        {
+            startGameTransform.position = Vector3.MoveTowards(startGameTransform.position, startGameTextVisiblePosition, Time.deltaTime * Speed);
+            selectStageTransform.position = Vector3.MoveTowards(selectStageTransform.position, selectStageTextVisiblePosition, Time.deltaTime * Speed);
+            quitGameTransform.position = Vector3.MoveTowards(quitGameTransform.position, quitGameTextVisiblePosition, Time.deltaTime * Speed);
+            backTransform.position = Vector3.MoveTowards(backTransform.position, backTextHidePosition, Time.deltaTime * Speed);
+        }
     }
 
 
@@ -61,11 +67,11 @@ public class MainMenuBehavior : MonoBehaviour {
         }
         else if (textComponent.tag == "SelectStage")
         {
-            inSelectLevel = true;
+            GameObject.FindGameObjectWithTag("MenuStatus").GetComponent<TextMesh>().text = "selectStage";
         }
         else if (textComponent.tag == "Back")
         {
-            inSelectLevel = false;
+            GameObject.FindGameObjectWithTag("MenuStatus").GetComponent<TextMesh>().text = "normal";
         }
         else
         {
