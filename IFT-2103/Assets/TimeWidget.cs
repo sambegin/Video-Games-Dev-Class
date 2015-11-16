bee5c2a5e737ff89b85dc8228ef12c9fc7b48913
David <david.arel@outlook.com>
2015-11-15 21:25:18 -0500

    Time since beginning of the level is shown on the HUD.

---
+++ IFT-2103/Assets/TimeWidget.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class TimeWidget : MonoBehaviour {
+
+    Text timeText;
+	// Use this for initialization
+	void Start () {
+        timeText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeText.text = Time.timeSinceLevelLoad.ToString();
+    }
+}
