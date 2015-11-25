b69ce3aaa2745674dc3e731411c6b32a8a7e19be
sam_begin <beginsam2@gmail.com>
2015-11-24 21:55:55 -0500

    big cleanup file + Clean code

---
+++ IFT-2103/Assets/Scripts/Camera/HitSpot.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitSpot : MonoBehaviour {
+
+    public Camera mainCamera;
+    public float HitSpotDistance = 20;
+
+	void Update () {
+        Vector3 centerOfCameraScreen = new Vector3(0.5f, 0.5f, 0);
+        Vector3 hitSpotPoint = mainCamera.ViewportPointToRay(centerOfCameraScreen).GetPoint(HitSpotDistance);
+        transform.position = hitSpotPoint;
+	}
+}
